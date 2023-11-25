56a515302b9f7950713644fabf8199aa07049e40
vbv-nyk <vbv.nyk.2003@gmail.com>
2023-11-25 23:04:37 +0530

    FIX relay malfunction

--- board_side.py
+++ board_side.py
@@ -2,12 +2,24 @@ import urequests
 import ujson
 import machine
 import time
+import network
+
+def smart_charge(charge):
+	if(charge == -1):
+		print("Device not connected to the internet")
+		return
+	if charge <= 90:
+		print("Charging your device")
+		relay_pin = machine.Pin(5, machine.Pin.OUT)
+	else:
+		print("Battery high, not charging")
+		relay_pin = machine.Pin(5, machine.Pin.IN)
 
 
 def get_battery():
     try:
-        response = urequests.get('http://localhost:8080/battery-stats')
-        if response.status_code == 200:
+	response = urequests.get('http://192.168.0.108:8080/battery-stats',timeout=5000)
+	if response.status_code == 200:
             data = ujson.loads(response.text)
             print("Response Type:", type(response))
             print("Response Text:", response.text)
@@ -17,22 +29,31 @@ def get_battery():
                 return data['battery']
             else:
                 print("No 'battery' field in the response")
+                return -1
         else:
             print("Request failed with status code:", response.status_code)
+            return -1
 
     except Exception as e:
         print("Exception occurred:", e)
 
 
-def charge_or_not(charge):
-    relay_pin = machine.Pin(5, machine.Pin.OUT)
-    relay_pin.value(0)
-    while response == 1:
-        if charge <= 60 and status == False:
-            relay_pin.value(1)
-        elif charge == 95 and status == True:
-            relay_pin.value(0)
+ssid = "ACT102618905398_2g"
+password = "52996826"
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+while not wlan.isconnected():
+    print("Attempting to connect")
+    pass
+
+print("Connected to WiFi")
 
 
-charge = get_battery()
-charge_or_not(charge)
+while(True):
+	charge = get_battery()
+	print(charge)
+	smart_charge(charge)
+	time.sleep(2)
